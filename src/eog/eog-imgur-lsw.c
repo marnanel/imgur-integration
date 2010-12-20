b10fdbc8f3dca3e65fba51b93b6b0708cd37e255
Thomas Thurman <tthurman@gnome.org>
2010-12-19 21:59:10 -0500

    Add status messages properly

--- src/eog/eog-imgur-lsw.c
+++ src/eog/eog-imgur-lsw.c
@@ -24,5 +24,33 @@ lsw_list_services (LswListServicesCallback *callback)
 		callback);
 }
 
+static void
+lsw_post_to_service_cb (SwClientService *service,
+	const GError *error,
+	gpointer userdata)
+{
+	LswPostToServiceCallback *cb = userdata;
+
+	cb (error);
+
+	/* FIXME do we free the service or what */
+}
+
+void
+lsw_post_to_service (gchar *service_name,
+	gchar *status,
+	LswPostToServiceCallback *cb)
+{
+	SwClient *client = sw_client_new ();
+	SwClientService *service = sw_client_get_service (client,
+                                        service_name);
+
+	sw_client_service_update_status (service,
+                                 lsw_post_to_service_cb,
+                                 status,
+                                 cb);
+
+}
+
 /* EOF eog-imgur-lsw.c */
 
--- src/eog/eog-imgur-lsw.h
+++ src/eog/eog-imgur-lsw.h
@@ -4,11 +4,13 @@
 #include <glib.h>
 
 typedef void (LswListServicesCallback) (const GList *services);
+typedef void (LswPostToServiceCallback) (const GError *error);
 
 void lsw_list_services (LswListServicesCallback *cb);
 
 void lsw_post_to_service (gchar *service_name,
-	gchar *message);
+	gchar *message,
+	LswPostToServiceCallback *cb);
 
 #endif
 
