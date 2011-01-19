de3bd5542569790b42c01ead85ae6c356edc20db
Thomas Thurman <tthurman@gnome.org>
2011-01-19 17:55:15 -0500

    Split out unit test of parser

--- src/dbus/parse.c
+++ src/dbus/parse.c
@@ -22,27 +22,6 @@
 #include <string.h>
 #include "parse.h"
 
-#ifdef TEST
-
-/* test data */
-
-char *fail = "<rsp stat=\"fail\">\n"
-  "<error_code>1000</error_code>\n"
-  "<error_msg>No image selected</error_msg>\n"
-  "</rsp>\n";
-
-char *pass = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-  "<rsp stat=\"ok\">\n"
-  "<image_hash>cxmHM</image_hash>\n"
-  "<delete_hash>NNy6VNpiAA</delete_hash>\n"
-  "<original_image>http://imgur.com/cxmHM.png</original_image>\n"
-  "<large_thumbnail>http://imgur.com/cxmHMl.png</large_thumbnail>\n"
-  "<small_thumbnail>http://imgur.com/cxmHMs.png</small_thumbnail>\n"
-  "<imgur_page>http://imgur.com/cxmHM</imgur_page>\n"
-  "<delete_page>http://imgur.com/delete/NNy6VNpiAA</delete_page>\n"
-  "</rsp>\n";
-#endif
-
 struct parseresults {
   gboolean passed;
   gboolean take_text;
@@ -152,41 +131,3 @@ parse_imgur_response(char *xml, gboolean *success)
   return g_list_reverse (results.details);
 }
 
-#ifdef TEST
-
-static gchar *
-join(GList *list)
-{
-   if (list==NULL)
-     {
-       return g_strdup("");
-     }
-   else
-     {
-       char *rest = join(list->next);
-       char *current = g_strdup_printf("%s%s%s",
-                         (gchar*) list->data,
-                         *rest ? "," : "",
-                         rest);
-
-       g_free (rest);
-
-       return current;
-     }
-}
-
-int
-main()
-{
-  char *message;
-  GList *result;
-  gboolean success;
-
-  result = parse_imgur_response(fail, &success);
-  printf("FAIL: Result=%d, message=%s\n", success, join(result));
-
-  result = parse_imgur_response(pass, &success);
-  printf("PASS: Result=%d, message=%s\n", success, join(result));
-}
-
-#endif
