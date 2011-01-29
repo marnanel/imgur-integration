5eef29f5c06e9a65cd8dd57134a8d0d0293a68df
marnanel <marnanel@marnanel-desktop.(none)>
2011-01-28 19:15:04 -0500

    apply coding conventions

--- src/sharing/plugin.c
+++ src/sharing/plugin.c
@@ -21,18 +21,9 @@
 #include <sharing-http.h>
 #include <osso-log.h>
 
-/**
- * test:
- * @account: #SharingAccount to be tested
- * @con: Connection used
- * @dead_mans_switch: Turn to %FALSE at least every 30 seconds.
- *
- * Test if #SharingAccount is valid.
- *
- * Returns: #SharingPluginInterfaceTestAccountResult
- */
-SharingPluginInterfaceAccountValidateResult validate (SharingAccount* account,
-    ConIcConnection* con, gboolean *cont, gboolean* dead_mans_switch)
+SharingPluginInterfaceAccountValidateResult
+validate (SharingAccount* account,
+          ConIcConnection* con, gboolean *cont, gboolean* dead_mans_switch)
 {
 	/* there are no accounts; it's always valid */
 	return TRUE;
@@ -42,7 +33,6 @@ SharingPluginInterfaceSendResult
 send (SharingTransfer* transfer,
       ConIcConnection* con, gboolean* dead_mans_switch)
 {
-
     SharingEntry *entry = sharing_transfer_get_entry (transfer);
 
     for (GSList* p = sharing_entry_get_media (entry); p; p=g_slist_next(p))
@@ -64,110 +54,50 @@ send (SharingTransfer* transfer,
     return SHARING_SEND_SUCCESS;
 }
 
-/**
- * sharing_plugin_interface_init:
- * @dead_mans_switch: What?
- *
- * Initialize interface
- *
- * Returns: 0
- */
-guint sharing_plugin_interface_init (gboolean* dead_mans_switch)
+guint
+sharing_plugin_interface_init (gboolean* dead_mans_switch)
 {
-    ULOG_DEBUG_L("sharing_manager_plugin_interface_init");
     return 0;
 }
 
-/**
- * sharing_plugin_interface_uninit:
- * @dead_mans_switch: What?
- *
- * Uninitialize interface
- *
- * Returns: 0
- */
-guint sharing_plugin_interface_uninit (gboolean* dead_mans_switch)
+guint
+sharing_plugin_interface_uninit (gboolean* dead_mans_switch)
 {
-    ULOG_DEBUG_L("sharing_manager_plugin_interface_uninit");
     return 0;
 }
 
-/**
- * sharing_plugin_interface_send:
- * @transfer: Transfer to be send
- * @con: Connection used
- * @dead_mans_switch: 
- *
- * Send interface.
- *
- * Returns: Result of send
- */
-SharingPluginInterfaceSendResult sharing_plugin_interface_send
-    (SharingTransfer* transfer, ConIcConnection* con,
+SharingPluginInterfaceSendResult
+sharing_plugin_interface_send (SharingTransfer* transfer, ConIcConnection* con,
     gboolean* dead_mans_switch)
 {
-    ULOG_DEBUG_L ("sharing_plugin_interface_send");
     SharingPluginInterfaceSendResult ret_val = SHARING_SEND_ERROR_UNKNOWN;
     ret_val = send (transfer, con, dead_mans_switch);
     return ret_val;
 }
 
-/**
- * sharing_plugin_interface_account_setup:
- * @transfer: #SharingTransfer send
- * @service: #SharingService
- * @worked_on: Connection used
- * @osso_context_t: Osso context
- *
- * Send interface
- *
- * Returns: Result of account setup
- */
-SharingPluginInterfaceAccountSetupResult sharing_plugin_interface_account_setup
-    (GtkWindow* parent, SharingService* service, SharingAccount** worked_on,
-    osso_context_t* osso)
+SharingPluginInterfaceAccountSetupResult
+sharing_plugin_interface_account_setup (GtkWindow* parent,
+		SharingService* service,
+		SharingAccount** worked_on,
+		osso_context_t* osso)
 {
-    ULOG_DEBUG_L ("sharing_plugin_interface_account_setup");
-    SharingPluginInterfaceAccountSetupResult ret = 0;
-    return ret;
+    return 0;
 }
 
-/**
- * sharing_manager_plugin_interface_account_validate:
- * @account: Account tested
- * @con: Connection used to test account
- * @dead_mans_switch: 
- *
- * Validates account information.
- *
- * Returns: Result of account validation
- */
 SharingPluginInterfaceAccountValidateResult
 sharing_plugin_interface_account_validate (SharingAccount* account, 
     ConIcConnection* con, gboolean *cont, gboolean* dead_mans_switch)
 {
-    ULOG_DEBUG_L ("sharing_plugin_interface_account_validate");
     SharingPluginInterfaceAccountValidateResult ret_val = 0;
     ret_val = validate (account, con, cont, dead_mans_switch);
     return ret_val;
 }
 
-/**
- * sharing_plugin_interface_edit_account:
- * @account: Account tested
- * @con: Connection used to test account
- * @dead_mans_switch: 
- *
- * Edit account plugin implementation.
- *
- * Returns: Result of account edit
- */
 SharingPluginInterfaceEditAccountResult
-    sharing_plugin_interface_edit_account (GtkWindow* parent,
+sharing_plugin_interface_edit_account (GtkWindow* parent,
     SharingAccount* account, ConIcConnection* con, gboolean* dead_mans_switch)
 {
     SharingPluginInterfaceEditAccountResult ret = 0;
-    ULOG_DEBUG_L ("sharing_plugin_interface_edit_account");
     return ret;
 }
 
