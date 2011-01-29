fc5733f61a929a75c9db2ff109db857333aa1803
marnanel <marnanel@marnanel-desktop.(none)>
2011-01-28 19:08:42 -0500

    make some functions a bit saner

--- src/sharing/send.c
+++ src/sharing/send.c
@@ -16,44 +16,29 @@
 #include "send.h"
 #include "common.h"
 
-/**
- * send:
- * @account: #SharingTransfer to be send
- * @con: Connection used
- * @dead_mans_switch: Turn to %FALSE at least every 30 seconds.
- *
- * Sends #SharingTransfer to service.
- *
- * Returns: #SharingPluginInterfaceSendResult
- */
-SharingPluginInterfaceSendResult send (SharingTransfer* transfer,
-    ConIcConnection* con, gboolean* dead_mans_switch)
+SharingPluginInterfaceSendResult
+send (SharingTransfer* transfer,
+      ConIcConnection* con, gboolean* dead_mans_switch)
 {
-    SharingPluginInterfaceSendResult ret = SHARING_SEND_SUCCESS;
 
-    SharingEntry *entry = sharing_transfer_get_entry( transfer );
+    SharingEntry *entry = sharing_transfer_get_entry (transfer);
 
-    gint result = 0;
-
-    for (GSList* p = sharing_entry_get_media (entry); p != NULL; p = g_slist_next(p)) {
+    for (GSList* p = sharing_entry_get_media (entry); p; p=g_slist_next(p))
+    {
       SharingEntryMedia* media = p->data;
-      /* Process media */
-      if (!sharing_entry_media_get_sent (media)) {
-	/* Post media */
-	//result = my_send_task_post_function (my_send_task, media);
-	/* Process post result */
-	if (result == 0 /* EXAMPLE: MY_SEND_RESULT_SUCCESS */) {
-	  /* If success mark media as sent */
+
+      if (!sharing_entry_media_get_sent (media))
+      {
+	/* send it */
+	if (0)
+	{
 	  sharing_entry_media_set_sent (media, TRUE);
-	  /* And mark process to your internal data structure */
-	  //my_send_task->upload_done += sharing_entry_media_get_size (media); 
-	} else {
-	  /* We have sent the file in last sharing-manager call */
-	  //my_send_task->upload_done += sharing_entry_media_get_size (media);
 	}
       }
+
+      *dead_mans_switch = 0; /* keepalive */
     }
 
-    return ret;
+    return SHARING_SEND_SUCCESS;
 }
 
